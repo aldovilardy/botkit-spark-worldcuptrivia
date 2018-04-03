fe531d213aabb4075708bbfb7c876d9b081fe034
Diego Fernando Nieto <d.nieto@calltech.local>
2018-04-02 19:40:12 -0500

    New gift skill

--- skills/gift.js
+++ skills/gift.js
@@ -1,11 +1,11 @@
 //
-// Command: help
+// Command: gift
 //
 var emoji = require('node-emoji');
 module.exports = function (controller) {
 
-    controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
-        var text = "😊 I understand:";
+    controller.hears([/^gift$/], 'direct_message,direct_mention', function (bot, message) {
+        var text = "Nice day! Here your free sticker today.";
         text += "\n- " + bot.appendMention(message, "about") + ": shows metadata about myself 🤖";
         text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills 🤖";
         
